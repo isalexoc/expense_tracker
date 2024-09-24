1c0ec678c15142ef2a22869d802924f947a70d8e
Isaac Orraiz <isaac87usa@gmail.com>
2024-09-24 10:02:44 -0400

    deploying

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
+// Disable the no-var ESLint rule for the global declaration
+/* eslint-disable no-var */
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+/* eslint-enable no-var */
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
